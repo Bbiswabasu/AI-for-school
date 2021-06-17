820f38358f000185d54efb2aece4dfdd234539d2
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2021-06-17 15:23:22 +0530

    eval expression added

---
+++ src/Tree games/TreeGenerator.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class TreeGenerator
+{
+    int height,num_vars;
+    ArrayList<Pair> var_nodes,
+    public TreeGenerator(int height,int num_vars)
+    {
+        this.height=height;
+        this.num_vars=num_vars;
+    }
+    public static void main(String args[])throws IOException
+    {
+        System.out.println("DF");
+    }
+}
