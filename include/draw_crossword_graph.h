544441a33f39bd2bbf835f569b0760c0519bccc2
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2021-07-18 20:08:29 +0530

    draw crossword graph game added

---
+++ include/draw_crossword_graph.h
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DrawCrosswordGraph
+{
+public:
+	void startGame();
+};
---
+++ src/Crossword_games/draw_crossword_graph.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "crossword_generator.h"
+#include "draw_crossword_graph.h"
+#include "CSPify.h"
+using namespace std;
+
+void DrawCrosswordGraph::startGame()
+{
+    cout<<"Find all edges in the graph :\n";
+}
