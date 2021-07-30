c8c79c6560457e8ba4d122f78663aef2ce34e277
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2021-07-30 20:46:30 +0530

    updated

--- bindings.cpp
+++ bindings.cpp
@@ -10,32 +10,32 @@ EMSCRIPTEN_BINDINGS(dag_generator)
 {
     register_vector<string>("VectorString");
     register_vector<int>("VectorInt");
-    
+    register_vector<vector<int>>("VectorVectorInt");
+
     class_<DAGGenerator>("DAGGenerator")
-    .constructor<>()
-    .function("init", &DAGGenerator::init)
-    .function("generate_dag", &DAGGenerator::generate_dag)
-    .function("assign_content", &DAGGenerator::assign_content)
-    .function("assign_values", &DAGGenerator::assign_values)
-    .function("display_dag", &DAGGenerator::display_dag)
-    .property("num_nodes", &DAGGenerator::get_num_nodes, &DAGGenerator::set_num_nodes)
-    .property("num_vars", &DAGGenerator::get_num_vars, &DAGGenerator::set_num_vars)
-    .property("expressions", &DAGGenerator::get_expressions)
-    .property("content", &DAGGenerator::get_content)
-    .property("values", &DAGGenerator::get_values)
-    ;
+        .constructor<>()
+        .function("init", &DAGGenerator::init)
+        .function("generate_dag", &DAGGenerator::generate_dag)
+        .function("assign_content", &DAGGenerator::assign_content)
+        .function("assign_values", &DAGGenerator::assign_values)
+        .function("display_dag", &DAGGenerator::display_dag)
+        .function("do_all_tasks", &DAGGenerator::do_all_tasks)
+        .property("num_vars", &DAGGenerator::get_num_vars, &DAGGenerator::set_num_vars)
+        .property("num_nodes", &DAGGenerator::get_num_nodes, &DAGGenerator::set_num_nodes)
+        .property("adj", &DAGGenerator::get_adj)
+        .property("content", &DAGGenerator::get_content)
+        .property("values", &DAGGenerator::get_values)
+        .property("expressions", &DAGGenerator::get_expressions);
 
     class_<MatchExpression>("MatchExpression")
-    .constructor<>()
-    .function("init", &MatchExpression::init)
-    .function("compute_expressions", &MatchExpression::compute_expressions)
-    .function("startGame", &MatchExpression::startGame)
-    ;
+        .constructor<>()
+        .function("init", &MatchExpression::init)
+        .function("compute_expressions", &MatchExpression::compute_expressions)
+        .function("startGame", &MatchExpression::startGame);
 
     class_<ExpressionEvaluation>("ExpressionEvaluation")
-    .constructor<>()
-    .function("init", &ExpressionEvaluation::init)
-    .function("evaluate", &ExpressionEvaluation::evaluate)
-    .function("startGame", &ExpressionEvaluation::startGame)
-    ;
+        .constructor<>()
+        .function("init", &ExpressionEvaluation::init)
+        .function("evaluate", &ExpressionEvaluation::evaluate)
+        .function("startGame", &ExpressionEvaluation::startGame);
 };
